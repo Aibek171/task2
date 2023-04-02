82187456c94e1aa4908527a47625fe42b4cc7cfe
Aibek171 <129676487+Aibek171@users.noreply.github.com>
2023-04-02 19:54:46 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,11 +7,9 @@ from scipy.stats import norm
 chat_id = 1121374935 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    time = 62
     alpha = 1 - p
-    loc = x.mean()
+    loc = (2*x/time**2).mean()
     scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
     return loc - scale * norm.ppf(1 - alpha / 2), \
            loc - scale * norm.ppf(alpha / 2)
