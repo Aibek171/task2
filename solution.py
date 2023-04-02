13818b163ce90333037a0bf7c553844d6db2af84
Aibek171 <129676487+Aibek171@users.noreply.github.com>
2023-04-02 22:50:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,9 +8,9 @@ chat_id = 1121374935 # Ваш chat ID, не меняйте название пе
 
 def solution(p: float, x: np.array) -> tuple:
     time = 62
+    alpha = 1 - p
     n=len(x)
     z = x*2/time**2
     loc = np.mean(x)*2/time**2
     se = np.sqrt(np.sum((z - loc)**2)/(n-1)) / np.sqrt(n)
-    t_val = t.ppf(1-p/2, n-1)
-    return loc - se * t_val, loc + se * t_val
+    return loc + se * t.ppf(alpha/2, n-1), loc + se * t.ppf(1 - alpha/2, n-1)
