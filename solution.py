c9654d7250283eb19ad0242af3ab428b82d18850
Aibek171 <129676487+Aibek171@users.noreply.github.com>
2023-04-02 21:28:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import t
 
 
 chat_id = 1121374935 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     time = 62
-    alpha = 1 - p
-    loc = (2*x/time**2).mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    n=len(x)
+    z = x*2/time**2
+    loc = np.mean(x)*2/time**2
+    se = np.sqrt(np.sum((z - loc)**2)/(n-1)) / np.sqrt(n)
+    t_val = t.ppf(1-p/2, n-1)
+    return loc - se * t_val, loc + se * t_val
